8dfa4e4f5db302eecb5472f4d68c7f336a200895
lovelivesimply <1047723382@qq.com>
2017-12-26 17:38:55 +0800

    [dandan] add 04

---
+++ src/NHibernate/04_handle_one_to_many/src/Orm.Practice/Entities/Parent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentNHibernate.Conventions.Inspections;
+using FluentNHibernate.Mapping;
+
+namespace Orm.Practice.Entities
+{
+    public class Parent
+    {
+        public virtual Guid ParentId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual IList<Child> Children { get; set; }
+        public virtual bool IsForQuery { get; set; }
+    }
+
+    public class ParentMap : ClassMap<Parent>
+    {
+        public ParentMap()
+        {
+            #region Please modify the code to pass the test
+
+            Id(m => m.ParentId).GeneratedBy.GuidComb();
+            Map(m => m.Name);
+            HasMany(x => x.Children)
+                .Inverse()
+                .Cascade.All();
+            Map(m => m.IsForQuery);
+            Table("parent");
+            #endregion
+        }
+    }
+}
